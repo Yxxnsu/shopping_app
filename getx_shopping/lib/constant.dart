5ac5fa432034a92d62a84e273765bcb18f222f5a
Yxxnsu <dustn0121@naver.com>
2022-05-10 16:11:43 +0900

    [ADD] constants.dart

---
+++ getx_shopping/lib/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const String tableCertProduct = 'certProduct';
+const String columnName = 'name';
+const String columnImage = 'image';
+const String columnQuantity = 'quantity';
+const String columnPrice = 'price';
+const String columnProductId = 'productId';
+
+const CACHED_USER_DATA = 'CACHED_USER_DATA';
+
+const primaryColor = Color.fromRGBO(0, 197, 105, 1);
